34e73ec6c3d02dfb5bc29f596493e06b65735a5e
Benjamin Elser <belser@AUS-MBP-6487.local>
2020-01-29 17:40:45 -0600

    updated key creation

--- InsightVM/ivminstall.sh
+++ InsightVM/ivminstall.sh
@@ -3,3 +3,4 @@ wget -c http://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin
 chmod +x Rapid7Setup-Linux64.bin
 ./Rapid7Setup-Linux64.bin -q -Vfirstname=rapid -Vlastname=seven -Vcompany=r7 -Vusername=nxadmin -Vpassword1=ecadmin -Vpassword2=ecadmin
 systemctl start nexposeconsole.service
+init 6
