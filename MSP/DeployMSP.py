fdcab440b5c8465e794ba877282dec4b20771365
Ben Elser <benjamin.elser18@gmail.com>
2020-04-27 16:51:41 -0500

    Updated Deploy MSP and readme.md

---
+++ MSP/DeployMSP.py
@@ -0,0 +1,253 @@
+import botostubs, boto3, sys, os, stat, base64, time, json
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+ec2 = boto3.resource('ec2', region_name='us-east-2')
+client = boto3.client('ec2', region_name='us-east-2')
+iam = boto3.client('iam')
+ssm_client = boto3.client('ssm', region_name="us-east-2") 
+
+def ExecuteNetstat(i):
+    response = ssm_client.send_command(
+                InstanceIds=[
+                    i,
+                    ],
+                DocumentName="AWS-RunShellScript",
+                Parameters={
+                    'commands':[
+                        'netstat -plnt | grep 3790 && echo "True" || echo "False"'
+                    ]
+                }
+        )
+    
+    command_id = response['Command']['CommandId']
+    return command_id
+
+def GetNetstatCommandStatus(i, c, output=None):
+    # Handling api errors
+    if output == None:
+        try:
+            output = ssm_client.get_command_invocation(
+            CommandId = c,
+            InstanceId = i
+            )
+            if output == None:
+                GetNetstatCommandStatus(i, c, None)
+            else:
+                return output
+        except:
+            time.sleep(5)
+            GetNetstatCommandStatus(i, c, None)
+    else:
+        return output
+        
+def GetNetstatStatus(i):
+    comamnd_id = ExecuteNetstat(i)
+    output = GetNetstatCommandStatus(i, comamnd_id)
+    # Brute force this thing
+    while output == None:
+       output = GetNetstatCommandStatus(i, comamnd_id)
+    while (output['StatusDetails'] == 'InProgress') & (output['Status'] == 'InProgress'):
+        time.sleep(3)
+        output = GetNetstatCommandStatus(i, comamnd_id)
+    
+    results = output['StandardOutputContent'].split("\n")
+    for result in results:
+        if result == 'True':
+            return True
+        if result == 'False':
+            return False
+    
+def CheckService(i): 
+    result = GetNetstatStatus(i)
+    minutes = 1
+    while result != True:
+        if minutes == 40:
+            sys.exit(f"{bcolors.FAIL}Something went wrong while starting to start the NSC service.{bcolors.ENDC}")
+        print(f"{bcolors.WARNING}\nWaiting for NSC service to start. Total minutes: {minutes}{bcolors.ENDC}")
+        time.sleep(60)
+        minutes += 1
+        result = GetNetstatStatus(i)
+
+def CreatePreSharedKey(keyname, path):
+    if not os.path.exists(path):
+        print(f"{bcolors.OKGREEN}Creating %s{bcolors.ENDC}" % path)
+        os.mkdir(path)
+    os.chdir(path) 
+    key_file=f'{path}/{keyname}.pem'
+    print(f"{bcolors.WARNING}Attempting to create key{bcolors.ENDC}")
+    try:
+        key = ec2.create_key_pair(KeyName=keyname)
+        print(f"{bcolors.OKGREEN}Creating key name: {keyname} {bcolors.ENDC}")
+        print(f"{bcolors.WARNING}Checking to see if: {key_file} exists. {bcolors.ENDC}")
+        if not os.path.exists(key_file):
+            print(f"{bcolors.FAIL}{key_file} does not exists. {bcolors.ENDC}")
+            print(f"{bcolors.OKGREEN}Creating: {key_file}{bcolors.ENDC}")
+            outfile = open(key_file, 'w')
+            key_pair_out = str(key.key_material)
+            print(f"{bcolors.WARNING}Writing key: {keyname} to {key_file} {bcolors.ENDC}")
+            outfile.write(key_pair_out)
+            os.chmod(key_file, stat.S_IREAD)
+    except:
+        print(f"{bcolors.OKGREEN}Key name: {keyname} already exists. Make sure you have access to .pem{bcolors.ENDC}")
+               
+def SecurityGroupExists(SGName):
+    try:
+        security_groups = ec2.security_groups
+        for sg in security_groups.all():
+            if sg.group_name == SGName:
+                return True
+            else:
+                return False
+    except:
+        return False
+    
+def CreateMetasploitSecurityGroup():
+    try:
+        if not SecurityGroupExists('Metasploit'):
+            print(f"{bcolors.OKGREEN}Creating Metasploit Security Group{bcolors.ENDC}")
+            # Create Security Group autorizing ssh and 3780 to console
+            sg = ec2.create_security_group(GroupName='Metasploit', Description="Metasploit_Connectivity")
+            sg.authorize_ingress(
+                IpPermissions=[
+                    {
+                        'FromPort': 22,
+                        'IpProtocol': 'tcp',
+                        'IpRanges': [
+                            {
+                                'CidrIp': '0.0.0.0/0',
+                                'Description': 'ssh'
+                            },
+                        ],
+                        'ToPort': 22,
+                    }
+                ],
+            )
+        else:
+            print(f"{bcolors.OKGREEN}Metasploit Security Group alrady exists{bcolors.ENDC}")
+    except:
+        print(f"{bcolors.WARNING}Validate Metasploit Security Group attached correctly EC2 Instance.{bcolors.ENDC}")
+
+def GetPublicIp(instanceid):
+    time.sleep(5)
+    os.system('clear')
+    print(f"{bcolors.OKGREEN}Fetching public ipv4 address for instance: {instanceid}{bcolors.ENDC}")
+    time.sleep(10)
+    publicIpv4 = ""
+    instances = ec2.instances
+    for i in instances.all():
+        if i.id == instanceid:
+            publicIpv4 += i.public_ip_address
+            os.system('clear')
+            return publicIpv4
+
+def CreateSSMMangedRole():
+    AssumeRolePolicyDocument={
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": [
+                        "ec2.amazonaws.com"
+                    ]
+                },
+                "Action": [
+                    "sts:AssumeRole"
+                ]
+            }
+        ]
+    }
+    try:
+        print(f"{bcolors.WARNING}Checking if SSMManaged Role exists{bcolors.ENDC}")
+        role = iam.get_role(RoleName='SSMManaged')
+        print(f"{bcolors.OKGREEN}SSMManaged Role FOUND{bcolors.ENDC}")
+    except:
+        print(f"{bcolors.FAIL}SSMManaged Role could not be found{bcolors.ENDC}")
+        print(f"{bcolors.OKBLUE}Attempting to create SSMManged Role{bcolors.ENDC}")
+        role = iam.create_role(
+        Path='/',
+        RoleName='SSMManaged',
+        AssumeRolePolicyDocument=json.dumps(AssumeRolePolicyDocument)
+        ) 
+        result = iam.attach_role_policy(RoleName='SSMManaged',PolicyArn='arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore')
+        instance_profile = iam.create_instance_profile(InstanceProfileName='SSMManaged',Path='/')
+        iam.add_role_to_instance_profile(InstanceProfileName='SSMManaged', RoleName='SSMManaged')
+        print(f"{bcolors.OKGREEN}SSMManaged Role was created successfully{bcolors.ENDC}")
+
+def CreateEC2(keypair, path):
+    # Read in shell script to execute on startup --runs as root
+    # if not os.path.exists(f"{path}/ivminstall.sh"):
+    #     print(f"{bcolors.FAIL}ivminstall.sh not found.{bcolors.ENDC}")
+    #     sys.exit(f"{bcolors.FAIL}ivminstall.sh needs to be in the same folder as this script.{bcolors.ENDC}")
+    # os.chdir(path)
+    # f = open('./ivminstall.sh', encoding='ascii')
+    # startUpScript = f.read()
+    print(f"{bcolors.OKGREEN}Creating EC2{bcolors.ENDC}")
+    instance = ec2.create_instances(
+        ImageId='ami-00f7390b60c41a3c0',
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t2.2xlarge',
+        KeyName=keypair,
+        SecurityGroupIds=[
+            'Metasploit',
+        ],
+        #UserData=startUpScript,
+        IamInstanceProfile={
+            'Name':'SSMManaged'
+        }
+    )
+    return instance[0].id
+
+def HookUpIAMProfile(i):
+    try:
+        print(f"{bcolors.OKGREEN}Attempting to connect IAM Role SSMManaged to EC2: {i}{bcolors.ENDC}")
+        ip = iam.get_instance_profile(InstanceProfileName="SSMManaged")
+        client.associate_iam_instance_profile(IamInstanceProfile={'Arn':ip['InstanceProfile']['Arn']}, InstanceId=i)
+        print(f"{bcolors.OKGREEN}Successfully connected IAM Role SSMManaged Role to EC2: {i}{bcolors.ENDC}")
+    except:
+        print(f"{bcolors.FAIL}SSMManaged Role could not be attached to EC2: {i}{bcolors.ENDC}")
+
+def DisconnectIAMProfile(i):
+    try:
+        ip = iam.get_instance_profile(InstanceProfileName="SSMManaged")
+        associations = client.describe_iam_instance_profile_associations()
+        for assoc in associations['IamInstanceProfileAssociations']:
+            if assoc['InstanceId'] == i:
+                print(f"{bcolors.OKGREEN}Attempting to disconnect IAM Role SSMManaged Role from EC2: {i}{bcolors.ENDC}")
+                client.disassociate_iam_instance_profile(AssociationId=assoc['AssociationId'])
+                print(f"{bcolors.OKGREEN}Successfully disconnected IAM Role SSMManaged Role from EC2: {i}{bcolors.ENDC}")
+    except:
+        print(f"{bcolors.FAIL}SSMManaged Role could not be disconnected to EC2: {i}{bcolors.ENDC}")
+
+def WaitForInstance(i):
+    time.sleep(5)
+    os.system('clear')
+    print(f"{bcolors.WARNING}\n\nWaiting for instance: {i} to be ready. You should go grab a coffee{bcolors.ENDC}")
+    waiter = client.get_waiter('instance_status_ok')
+    waiter.wait(InstanceIds=[i])
+
+def Main():
+    scriptDir = os.environ['PWD']
+    path =f'{os.environ["HOME"]}/.aws'
+    keypair_name = "ec2-keypair"
+    CreatePreSharedKey(keypair_name, path)
+    CreateMetasploitSecurityGroup()
+    CreateSSMMangedRole()
+    instanceid = CreateEC2(keypair_name, scriptDir)
+    WaitForInstance(instanceid)
+    # CheckService(instanceid)
+    publicipv4 = GetPublicIp(instanceid)
+    cmd = f"ssh -L 3790:localhost:3790 -N -f ec2-user@{publicipv4} -i PATHTOYOURKEY.pem"
+    print(f"{bcolors.OKGREEN}\nCreate SSH Tunnel (copy and paste):\n\t{cmd}\n\nUSING your browser navigate to:\n\thttps://{publicipv4}:3790{bcolors.ENDC}")
+
+Main()
--- MSP/readme.md
+++ MSP/readme.md
@@ -1,32 +1,76 @@
-# Metasploit Pro
+# AWS Attack Lab Build 
+
+## Importing Vulnerable Machines
+1. Identify supported [OS's](https://docs.aws.amazon.com/vm-import/latest/userguide/vm-import-ug.pdf) 
+    - Ensure vulnerable
+    - Test locally and make any modifications needed prior to export
+2. Export VM from virtual enviornment to OVF 2.0
+3. Setup S3 buckets and AWS roles according to [this docutment](https://docs.aws.amazon.com/vm-import/latest/userguide/vm-import-ug.pdf)
+4. Upload exported OVA to S3:
+```bash
+aws s3 cp Server2016.ova s3://import-vmdks/Server2016.ova
+```
+5. Create JSON conversion template (server2016.json)
+```json
+[
+  {
+    "Description": "Server2016 OVA",
+    "Format": "ova",
+    "UserBucket": {
+        "S3Bucket": "import-vmdks",
+        "S3Key": "Server2016.ova"
+    }
+}]
+```
+6. Convert OVF
+```bash
+aws ec2 import-image --description "Server2017" --disk-containers "file:///Users/myuser/server2016.json"
+```
+
+## Metasploit Pro High Level 
 Quick start on getting MSP up and running
 
-## Step 1: Download Installer
+1. Download Installer
 Windows: [64-Bit](https://downloads.metasploit.com/data/releases/metasploit-latest-windows-installer.exe)
 <br />
 Linux: [64-Bit](https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run)
 
-## STEP 2: Install
+2. Install
 Metasploit uses some of the same techniques as malware and malicious attackers to audit your security. For Metasploit to run properly, you must switch off anti-virus solutions and your local firewall before the installation and during its use. If you are not comfortable turning off defenses on this computer, consider installing Metasploit on a dedicated virtual machine (set virtual machine NIC to bridged mode). Ubuntu works great and doesn't incur any OS licensing fees.
 
 Once the download is complete, run the installer and follow the step by step instructions. This installer contains all Metasploit editions.
 
-## STEP 3 or sometime: Activate
+3. Sometime: Activate license
 *Get-LicenseKey*
 
-## Step 4: Restart Service
+4. Restart Service
 ```bash
-/opt/metasploit/ctlscript.sh restart
+sudo /opt/metasploit/ctlscript.sh restart
 ```
-
-## Check service is listening
+5. Check service is listening
 ```bash
 netstat -anop | grep 3790
 ```
-
-## Setup ssh tunnel with local port forward to access web front end
+6. Setup ssh tunnel with local port forward to access web front end
 ```bash
 ssh -L 3790:localhost:3790 -N -f ec2-user@PublicDNS -i key_pairs.pem
 ```
-### Browse to:
+7. Browse to:
 https://localhost:3790
+
+
+## Kali
+[DeployMSP.py](./DeployMSP.py) Deploys Kali. MSP installer is interactive so go ahead and step through installation. 
+```bash
+sudo apt-get update -y && apt-get upgrade -y
+wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run -O /tmp/installer.run && sudo chmod 755 /tmp/installer.run
+sudo /tmp/installer.run
+sudo /opt/metasploit/ctlscript.sh restart
+```
+
+## Deploy Target Systems
+1. Deploy domain
+2. Add systems to domain
+3. Ensure networking from VPC to internet and from VPC (target) to VPC (attacker) is good to go
+4. Setup scenerios and stage accounts
+5. Iterate step 4.
